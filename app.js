4e543ecfc6ab79006666b30884c2cf794ffc5851
Shiva16-04 <shivakumaryedla1999@gmail.com>
2025-09-27 13:57:09 +0530

    Added error handling

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 import { generateDiscountCardInterface, addEventListeners } from "./helper.js";
 
 
-
 //calling the final functions
-generateDiscountCardInterface();
-addEventListeners();
+try{
+    generateDiscountCardInterface();
+    addEventListeners();
+}catch (error) {
+    console.log("Exception occured at :", error);
+}
 
 
 
